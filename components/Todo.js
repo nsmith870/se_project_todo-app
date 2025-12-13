79f58d710418504b08e90e19ce509eab008110e9
Nathan Smith <nsmith.life870@gmail.com>
2025-12-13 14:10:51 -0600

    ToDo App First commit

---
+++ components/Todo.js
@@ -0,0 +1,67 @@
+class Todo {
+  constructor(data, templateSelector) {
+    this._data = data;
+    this._todoTemplate = document.querySelector(templateSelector).content;
+  }
+
+  _setEventListeners() {
+    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = todoCheckboxEl.checked;
+      console.log(
+        `Todo "${this._data.name}" completed state:`,
+        this._data.completed
+      );
+    });
+
+    todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+      console.log(`Todo "${this._data.name}" deleted`);
+    });
+  }
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+
+    this._todoCheckboxEl.checked = this._data.completed;
+
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _generateDueDate() {
+    const todoDate = this._todoElement.querySelector(".todo__date");
+
+    const dueDate = new Date(this._data.date);
+
+    if (!isNaN(dueDate)) {
+      todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  getView() {
+    this._todoElement = this._todoTemplate
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    this._generateCheckboxEl();
+    this._setEventListeners();
+    this._generateDueDate();
+
+    return this._todoElement;
+  }
+}
+
+export default Todo;
